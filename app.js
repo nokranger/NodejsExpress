9e120e3d9fb37491a1d880e2c5e59b9c91976ada
nokranger <anusorn.thavornporn@gmail.com>
2019-03-09 17:50:35 +0700

    mongodb

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 var logger = require('morgan');
 const hbs = require('express-handlebars')
+const expressValidator = require('express-validator')
+const expressSession = require('express-session')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,10 +20,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret : 'max' ,saveUninitialized : false,resave : false}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool, hub', condition : true,anyArray:[1,2,3] });
+  res.render('index', { title: 'Form Validation', success : req.session.success, errors : req.session.errors });
+  req.session.errors = null 
 });
 router.get('/test/:id',function(req,res,next){
   res.render('test',{output:req.params.id})
@@ -11,5 +12,18 @@ router.get('/test/:id',function(req,res,next){
 router.post('/test/submit',function(req,res,next){
   let id = req.body.id
   res.redirect('/test/' + id)  
+})
+router.post('/submit',function(req,res,next){
+  req.check('email', 'Invalid email address').isEmail()
+  req.check('password', 'Password is invalid').isLength({min:4}).equals(req.body.confirmPassword)
+
+  let errors = req.validationErrors()
+  if(errors){
+    req.session.errors = errors
+    req.session.success = false
+  }else {
+    req.session.success = true
+  }
+  res.redirect('/')
 })
 module.exports = router;
