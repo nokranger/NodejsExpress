0dc725f7fad7a0b0aa7d97d0353e4af46cfeeed1
nokranger <anusorn.thavornporn@gmail.com>
2019-03-10 16:37:03 +0700

    update delete mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const mongo = require('mongodb')
+const mongo = require('mongodb').MongoClient
+const objectID = require('mongodb').ObjectID
 const assert = require('assert')
 
 const url = 'mongodb://localhost:27017/test'
@@ -39,9 +40,35 @@ router.post('/insert',function(req,res,next){
   })
 })
 router.post('/update',function(req,res,next){
-
+  let item = {
+    title : req.body.title,
+    content : req.body.content,
+    author : req.body.author
+  }
+  let id = req.body.id
+  mongo.connect(url,function(err,db){
+    assert.equal(null,err)
+    db.collection('user-data').updateOne({"_id" : objectID(id)},{$set : item},function(err,result){
+      assert.equal(null,err)
+      console.log('Item updated')
+      db.close()
+    })
+  })
 })
 router.post('/delete',function(req,res,next){
-
+  let item = {
+    title : req.body.title,
+    content : req.body.content,
+    author : req.body.author
+  }
+  let id = req.body.id
+  mongo.connect(url,function(err,db){
+    assert.equal(null,err)
+    db.collection('user-data').deleteOne({"_id" : objectID(id)},{$set : item},function(err,result){
+      assert.equal(null,err)
+      console.log('Item deleted')
+      db.close()
+    })
+  })
 })
 module.exports = router;
